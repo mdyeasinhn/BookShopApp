29eaedf4fa182450f4515d01d6052f6a0a66eea6
mdyeasinhn <mdyeasinhn@gmail.com>
2025-02-22 20:05:38 +0600

    redux initial setup done

---
+++ client/src/pages/auth/Login.tsx
@@ -0,0 +1,11 @@
+
+
+const Login = () => {
+    return (
+        <div>
+            i am login
+        </div>
+    );
+};
+
+export default Login;
---
+++ client/src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+
+
+const home = () => {
+    return (
+        <div>
+            I am home
+        </div>
+    );
+};
+
+export default home;
