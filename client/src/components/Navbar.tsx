be1a9810c8dff903c8a75ae0328bf8f1920d2548
mdyeasinhn <mdyeasinhn@gmail.com>
2025-02-26 20:29:46 +0600

    navbar updated

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,28 +1,54 @@
-import {
-  Menubar,
-  MenubarMenu,
-
-  MenubarTrigger,
-} from "@/components/ui/menubar"
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
-
-// Ensure this points to your custom Container component
-
+import { AiOutlineMenu } from "react-icons/ai";
+import logo from '../assets/images/circular-wheat-with-opened-book-and-pen-writer-inside-free-vector.jpg'
 const Navbar = () => {
-
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="fixed w-full flex justify-center z-10  ">
-        <Menubar>
-          <MenubarMenu>
-           <Link to="/"> <MenubarTrigger>Home</MenubarTrigger></Link>
-           <Link to="/"> <MenubarTrigger>Books</MenubarTrigger></Link>
-           <Link to="/"> <MenubarTrigger>About</MenubarTrigger></Link>
-           <Link to="/login"> <MenubarTrigger>Login</MenubarTrigger></Link>
-            
-          </MenubarMenu>
-        </Menubar>
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <div className="container mx-auto flex items-center justify-between">
+          {/* Logo */}
+          <Link to="/">
+            <img
+              src={logo}
+              alt="logo"
+              width="100"
+              height="100"
+            />
+          </Link>
+
+          {/* Dropdown Menu */}
+          <div className="relative">
+            <div
+              onClick={() => setIsOpen(!isOpen)}
+              className="p-4 border-[1px] border-neutral-200 flex items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+            >
+              <AiOutlineMenu />
+            </div>
+
+            {isOpen && (
+              <div className="absolute right-0 top-12 w-40 bg-white rounded-xl shadow-md text-sm">
+                <div className="flex flex-col cursor-pointer">
+                  <Link
+                    to="/dashboard"
+                    className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+                  >
+                    Dashboard
+                  </Link>
+                  <Link
+                    to="/login"
+                    className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+                  >
+                    Login
+                  </Link>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
